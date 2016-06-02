cf2ac8f71de705aa7ab3f4a0ffda313b8071905b
auldnick <auldnick@gmail.com>
2016-06-02 12:38:36 +0300

    stick cta buttons panel to top of footer

--- js/diglab.js
+++ js/diglab.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-    $(".buttons").hide();
-    $(function () {
+  $(".buttons").hide();
+  $(function () {
     $(window).scroll(function(){
-        if($(this).scrollTop() > 100) {
-            $(".buttons").show();
-        } 
+      if($(this).scrollTop() > 100) {
+        $(".buttons").show();
+      }
     });
+    $(window).scroll(function() {
+      if ($(window).scrollTop() + $(window).height() >= $(document).height() - 200) {
+        $(".buttons").css({"bottom":300, "position":"absolute"});
+        
+      } else {
+        $(".buttons").css({"bottom":0, "position":"fixed"});
+      }
     });
+  });
 });
    
