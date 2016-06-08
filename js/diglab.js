ae7f654e603c6e64f2193482880e5cbbfe6b976c
auldnick <auldnick@gmail.com>
2016-06-08 19:20:33 +0300

    new version of responsive header
    added scroll-down pointer with jQuery script
    changed background in footer: image only on the right side with text on the left
    replaced font-awesome folder to a newer version
    changed header h1 and h3 size units to vw (testing... testing...)

--- js/diglab.js
+++ js/diglab.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+//CTA buttons panel shown on scroll
   $(".buttons").hide();
   $(function () {
     $(window).scroll(function(){
@@ -6,14 +8,23 @@ $(document).ready(function() {
         $(".buttons").show();
       }
     });
+
+    //CTA buttons panel attached to footer
     $(window).scroll(function() {
       if ($(window).scrollTop() + $(window).height() >= $(document).height() - 200) {
         $(".buttons").css({"bottom":300, "position":"absolute"});
-        
       } else {
         $(".buttons").css({"bottom":0, "position":"fixed"});
       }
     });
   });
+
+//smooth scroll to first content block
+  $("#scroll").on('click', function(event) {
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+      }, 800, function(){
+    });
+  });
 });
-   
