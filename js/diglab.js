8a751a5a6a521e418733a7cdc15d4c043f754044
auldnick <auldnick@gmail.com>
2016-06-24 18:12:32 +0300

    fix script bugs, add local fonts, add minor layout tweaks, rearrange main header

--- js/diglab.js
+++ js/diglab.js
@@ -1,30 +1,24 @@
-$(document).ready(function() {
-
-//CTA buttons panel shown on scroll
-  $(".buttons").hide();
-  $(function () {
-    $(window).scroll(function(){
-      if($(this).scrollTop() > 100) {
-        $(".buttons").show();
-      }
-    });
-
-    //CTA buttons panel attached to footer
-    $(window).scroll(function() {
-      if ($(window).scrollTop() + $(window).height() >= $(document).height() - 200) {
-        $(".buttons").css({"bottom":300, "position":"absolute"});
-      } else {
-        $(".buttons").css({"bottom":0, "position":"fixed"});
-      }
-    });
-  });
-
 //smooth scroll to first content block
-  $("#scroll").on('click', function(event) {
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-      }, 800, function(){
-    });
+$(".pointerdown").on('click', function(event) {
+  $('html, body').animate({
+    scrollTop: $(".scrollhere").offset().top
+    }, 800, function(){
   });
+});
+//CTA buttons panel shown on scroll
+$(".buttons").hide();
+$(function () {
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100) {
+      $(".buttons").show();
+    }
+  });
+});
+//CTA buttons panel attached to footer
+$(window).scroll(function() {
+  if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
+    $(".buttons").css({"bottom":300, "position":"absolute"});
+  } else {
+    $(".buttons").css({"bottom":0, "position":"fixed"});
+  }
 });
